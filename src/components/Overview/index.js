eb171995eb810ea0d113669b85081bb3ad3fbc53
Paltashka <paltashka50@gmail.com>
2020-06-08 19:29:59 +0300

    Redirect to youtube on gameplay video

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -11,21 +11,21 @@ const Overview = () => {
           The Crystal Storm has wreaked havoc upon the many races of the Hermeian Galaxy and it is down to
           the player to guide the races in their efforts to rebuild and survive.
         </p>
-
         <p className="overview__text overview__text--light">
           Fight and defend against Crystal Scourge as it works to corrupt every planet and living being that it encounters,
           explore your planet to secure valuable resources, and build thriving settlements as you work to cleanse your planet
           and defeat the Crystal Scourge.
         </p>
-
-        <Button
-          text="gameplay video"
-          layout={
-            <div className="play-round"> 
-              <div className="play-triangle"></div>
-            </div>
-          } 
-        />
+        <a href="https://www.youtube.com/watch?v=iPmxQ9hZkvU">
+          <Button
+            text="gameplay video"
+            layout={
+              <div className="play-round">
+                <div className="play-triangle"></div>
+              </div>
+            }
+          />
+        </a>
       </div>
     </div>
   );
