2081a40dad1993725f7fc69e7794ae056fcf484f
Paltashka <paltashka50@hmail.com>
2020-06-10 15:57:13 +0300

    lazy load

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
+// import HomePage from './pages/HomePage';
+// import AboutPage from './pages/AboutPage';
 import AccountPage from './pages/AccountPage';
 import EmailVerificationPage from './pages/EmailVerificationPage';
 
@@ -15,22 +15,27 @@ import muskernack from './assets/forms/3-muskernack-smaller-size.png';
 
 import './index.scss';
 
+const HomePage = lazy(() => import('./pages/HomePage'));
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+
 function App() {
   return (
     <HashRouter> 
       <Switch>
-        <Route path="/" component={HomePage} exact />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/createaccount" component={CreateAccount} />
-        <Route path="/login" component={LoginModal} />
-        <Route path="/thanks-register">
-          <ThanksModal img={raken} title="Thank you for Registering!" text={true} />
-        </Route>
-        <Route path="/thanks-download">
-          <ThanksModal img={muskernack} title="Thank you for downloading Hash Rush!" text={false} />
-        </Route>
-        <Route path="/account" component={AccountPage} />
-        <Route path="/emailVerification/:token" component={EmailVerificationPage} />
+        <Suspense fallback>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/createaccount" component={CreateAccount} />
+          <Route path="/login" component={LoginModal} />
+          <Route path="/thanks-register">
+            <ThanksModal img={raken} title="Thank you for Registering!" text={true} />
+          </Route>
+          <Route path="/thanks-download">
+            <ThanksModal img={muskernack} title="Thank you for downloading Hash Rush!" text={false} />
+          </Route>
+          <Route path="/account" component={AccountPage} />
+          <Route path="/emailVerification/:token" component={EmailVerificationPage} />
+        </Suspense>
       </Switch>
     </HashRouter>
   );
