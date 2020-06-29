571875e058b01f149cb8efb66d3c195199235648
Paltashka <paltashka50@gmail.com>
2020-06-29 16:49:34 +0300

    Latest news now loaded from medium api

--- src/components/Community/index.js
+++ src/components/Community/index.js
@@ -1,31 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import './index.scss';
+import { Parallax } from 'react-scroll-parallax';
+import moment from 'moment';
+
 import ButtonRounded from './ButtonRounded';
+import NewsBlock from '../News/NewsBlock';
+
 import facebook from '../../assets/social-media/facebook.svg';
 import tw from '../../assets/social-media/twitter.svg';
 import discord from '../../assets/social-media/discord.svg';
 import telegram from '../../assets/social-media/tlgrm.svg';
-
-import bg from '../../assets/images/fores.png';
-
-import { Parallax } from 'react-scroll-parallax';
-
-import NewsBlock from '../News/NewsBlock';
 import readMore1 from '../../assets/images/readMore1.png';
 import readMore2 from '../../assets/images/readMore2.png';
 import readMore3 from '../../assets/images/readMore3.png';
 
+import bg from '../../assets/images/fores.png';
+
+import './index.scss';
 
 const ParallaxImg = () => {
   return (
-    <Parallax y={[25, -5]} styleInner={{transform: 'translate3d(0%, 24.1057%, 0px)'}}>
+    <Parallax y={[25, -5]} styleInner={{ transform: 'translate3d(0%, 24.1057%, 0px)' }}>
       <img className="community__bg" src={bg} />
     </Parallax>
   );
 };
 
+function toText(node) {
+  let tag = document.createElement('div');
+  tag.innerHTML = node;
+  node = tag.innerText;
+  return node;
+}
+
+function shortenText(text, startingPoint, maxLength) {
+  return text.length > maxLength ?
+    text.slice(startingPoint, maxLength) :
+    text;
+}
+
 const Community = () => {
   const [screnWidth, setScreenWidth] = useState(window.innerWidth);
+  const [posts, setPosts] = useState();
+
   const handleResize = () => setScreenWidth(window.innerWidth);
 
   useEffect(() => {
@@ -34,15 +50,29 @@ const Community = () => {
     return () => {
       window.removeEventListener('resize', handleResize);
     }
-  }, [handleResize, screnWidth])
+  }, [handleResize, screnWidth]);
+
+  useEffect(async () => {
+    await fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/vz-games')
+      .then((res) => res.json())
+      .then((data) => {
+        const res = data.items;
+        const posts = res.filter(item => item.categories.length > 0);
+        console.log(posts)
+        setPosts(posts.slice(0, 3));
+      });
+  }, []);
 
   return (
     <div className="community">
       <div className="news__blocks">
-          <NewsBlock imgUrl={readMore1} date={'6 Jun 2020'} title={'Hash Rush Progress Update — May 2020'} text={'It has been quite a while since we last wrote to you talking about what we’ve been working on...'} href={'https://medium.com/@Hash_Rush/hash-rush-progress-update-may-2020-1d205d9112f8'} />
-          <NewsBlock imgUrl={readMore2} date={'28 Feb 2020'} title={'Hash Rush February update'} text={'February has almost ended so we are back with a look at what we’ve been working on during this month.'} href={'https://medium.com/@Hash_Rush/hash-rush-february-update-e44a253e8b72?source=---------3------------------'} />
-          {(screnWidth > 1024 || screnWidth < 415) && <NewsBlock imgUrl={readMore3} date={'31 Jan 2020'} title={'Hash Rush in 2020'} text={'Hello everyone and welcome to our first news blast of 2020! With our last update — the Rise of the Crystal Titan...'} href={'https://medium.com/@Hash_Rush/hash-rush-in-2020-4efec0ffaa11?source=---------4------------------'} />}
-        </div>
+        {posts && posts.map((item, index) => (
+          <div key={index}>
+            <NewsBlock imgUrl={item.thumbnail} date={moment(item.pubDate).format('ll')} title={item.title} text={shortenText(toText(item.content), 0, 120) + '...'} href={item.link} />
+          </div>
+        ))}
+        {/* {(screnWidth > 1024 || screnWidth < 415) && <NewsBlock imgUrl={readMore3} date={'31 Jan 2020'} title={'Hash Rush in 2020'} text={'Hello everyone and welcome to our first news blast of 2020! With our last update — the Rise of the Crystal Titan...'} href={'https://medium.com/@Hash_Rush/hash-rush-in-2020-4efec0ffaa11?source=---------4------------------'} />} */}
+      </div>
       <ParallaxImg />
 
       <div className="community__content">
@@ -58,10 +88,10 @@ const Community = () => {
       <div className="community__about-pos">
         <h1 className="community__about heading">About Hash Rush</h1>
         <p className="about__text">
-            An online sci-fi/fantasy RTS set in the fictional Hermeian galaxy, Hash Rush sees players build,
-            fight and trade their way to victory as they grow and manage their small mining colony, battle monsters, and trade valuable items and resources.
-            Tasked with accumulating the much sought-after Crypto Crystals on their planet, successful players will earn themselves tokenized blockchain rewards as well
-            as in-game items, bonuses and other prizes.
+          An online sci-fi/fantasy RTS set in the fictional Hermeian galaxy, Hash Rush sees players build,
+          fight and trade their way to victory as they grow and manage their small mining colony, battle monsters, and trade valuable items and resources.
+          Tasked with accumulating the much sought-after Crypto Crystals on their planet, successful players will earn themselves tokenized blockchain rewards as well
+          as in-game items, bonuses and other prizes.
           </p>
       </div>
     </div>
