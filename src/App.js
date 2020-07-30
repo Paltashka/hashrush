d252b049b9b370f896d76bdf87410e3995ec0f1f
Andriy Yurchuk <paltashka50@gmail.com>
2020-07-30 14:39:31 +0300

    +

--- src/App.js
+++ src/App.js
@@ -66,9 +66,9 @@ function App({ signInByToken }) {
           <Route path="/resetPassword" component={CompleteResetPasswordPage} />
           <Route path="/bundles" component={BundlesPage}/>
           {/*<Route path="/bundle/:id" component={BundlePage} />*/}
-          <Route path="/bundle/melighar" component={MeligharPage} />
-          <Route path="/bundle/carax" component={CaraxPage} />
-          <Route path="/bundle/farion" component={FarionPage} />
+          {/*<Route path="/bundle/melighar" component={MeligharPage} />*/}
+          {/*<Route path="/bundle/carax" component={CaraxPage} />*/}
+          {/*<Route path="/bundle/farion" component={FarionPage} />*/}
           <Route path="/thank-you" component={ThankYouPage}/>
         </Suspense>
       </Switch>
