ed00fc00af6f03241db0bbeba9ee2044bed97f0d
Andriy Yurchuk <paltashka50@gmail.com>
2020-07-30 10:29:27 +0300

    Add sticky purchase block

--- src/components/BundleHeader/index.js
+++ src/components/BundleHeader/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
 import Purchase from './Purchase';
 import BundleHero from './BundleHero';
+import Sticky from 'react-stickynode';
 
 const BundleHeader = ({heroName, heroText, heroImg, heroType, heroDetailText, items, bgImg, price}) => {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    const handleResize = () => setScreenWidth(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [handleResize, screenWidth]);
+
     return (
         <div className="bundle-header" style={{
             backgroundImage: `url('${bgImg}')`,
@@ -13,7 +26,15 @@ const BundleHeader = ({heroName, heroText, heroImg, heroType, heroDetailText, it
         }}>
             <BundleHero heroName={heroName} heroText={heroText} heroImg={heroImg} heroType={heroType}
                         heroDetailText={heroDetailText} items={items}/>
-            <Purchase heroName={heroName} heroImg={heroImg} price={price}/>
+            <div className="purchase__wrapper">
+                {screenWidth > 1442 ? (
+                    <Sticky enabled={true} top='.header' bottomBoundary={2800}>
+                        <Purchase heroName={heroName} heroImg={heroImg} price={price}/>
+                    </Sticky>
+                ) : (
+                    <Purchase heroName={heroName} heroImg={heroImg} price={price}/>
+                )}
+            </div>
         </div>
     );
 };
