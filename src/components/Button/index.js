2a6cab8c03a26416b017134519d6bf23f2d2a1cb
Andriy Yurchuk <paltashka50@gmail.com>
2020-08-07 16:25:39 +0300

    Fix download after page

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './index.scss';
 
-const Button = ({ classPosition, text, layout, type, onClick, isVerified, width }) => {
-  return (
-    <a href={isVerified ? `${process.env.REACT_APP_ProdUrl}/HashRush` : null} style={{ display: 'contents'}} download>
-      <div onClick={onClick} className={`button__border-black ${classPosition ? classPosition : ''}`}>
-        <div className="button__border-black-inside">
-          <button type={type} className="button__border" style={width ? {width: width} : {}}>
-            <span className="button welcome__button">{layout}{text}</span>
-          </button>
-        </div>
-      </div>
-    </a>
-  );
+const Button = ({classPosition, text, layout, type, onClick, isVerified, width}) => {
+    const handleClick = () => {
+       if (isVerified) {
+           setTimeout(() => {
+               window.location.href = `${process.env.REACT_APP_ProdUrl}/HashRush`;
+           }, 500)
+       }
+    }
+    return (
+        <a onClick={handleClick} style={{ display: 'contents'}}>
+            <div onClick={onClick} className={`button__border-black ${classPosition ? classPosition : ''}`}>
+                <div className="button__border-black-inside">
+                    <button type={type} className="button__border" style={width ? {width: width} : {}}>
+                        <span className="button welcome__button">{layout}{text}</span>
+                    </button>
+                </div>
+            </div>
+        </a>
+    );
 };
 
 export default Button;
